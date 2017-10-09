cb920aa93c6e147bc354d723197745b85f158b8c
Pasi Lampinen <plam@fl027.local>
2017-10-09 10:33:17 +0300

    Adds mapCategory storage key

--- app/constants/StorageKeys.js
+++ app/constants/StorageKeys.js
@@ -5,6 +5,7 @@ const ROOT_KEY = APP_STORAGE_KEY;
 const tokenKeys = {
   token: `${ROOT_KEY}:token`,
   city: `${ROOT_KEY}:city`,
+  mapCategory: `${ROOT_KEY}:mapCategory`,
 }
 
 export default tokenKeys
