b0081750db5b81087af995a44676f7f21548fea6
Pasi Lampinen <plam@fl027.local>
2017-11-04 12:13:27 +0200

    Auth logic updated

--- app/concepts/auth.js
+++ app/concepts/auth.js
@@ -1,9 +1,16 @@
 import { AsyncStorage } from 'react-native';
 import Auth0 from 'react-native-auth0';
 import { fromJS } from 'immutable';
+import md5 from 'blueimp-md5';
 
 import api from '../services/api';
-import { updateProfile, putUser } from '../concepts/registration';
+import {
+  updateProfile,
+  putUser,
+  fetchUser,
+  openLoginTeamSelector,
+  closeLoginTeamSelector,
+} from '../concepts/registration';
 import { fetchAppContent } from './app';
 import { getTeams } from '../reducers/team';
 import { createRequestActionTypes } from '../actions';
@@ -18,11 +25,17 @@ export const {
   REFRESH_TOKEN_SUCCESS,
   REFRESH_TOKEN_FAILURE
 } = createRequestActionTypes('REFRESH_TOKEN');
+export const SHOW_LOGIN_ERROR = 'SHOW_LOGIN_ERROR';
+export const HIDE_LOGIN_ERROR = 'HIDE_LOGIN_ERROR';
+export const SHOW_LOGIN_LOADER = 'SHOW_LOGIN_LOADER';
+export const HIDE_LOGIN_LOADER = 'HIDE_LOGIN_LOADER';
 
 const auth0 = new Auth0({ domain: AUTH0_DOMAIN, clientId: AUTH0_CLIENTID });
 
 // # Selectors
 export const isRefreshingToken = state => state.auth.get('isRefreshingToken', false);
+export const isLoginFailed = state => state.auth.get('isLoginFailed', false);
+export const isLoadingAppAuth = state => state.auth.get('isLoadingAppAuth', false);
 
 // # Actions
 
@@ -35,6 +48,11 @@ export const openLoginView = () => (dispatch, getState) => {
   const teams = getTeams(state);
   let tokens;
 
+  // Hide possible login error
+  dispatch({ type: HIDE_LOGIN_ERROR });
+  dispatch({ type: SHOW_LOGIN_LOADER });
+  dispatch(startLoginProcess());
+
   auth0
   .webAuth
   .authorize({ scope: 'openid profile email'/*, audience: `https://${AUTH0_DOMAIN}/userinfo` */})
@@ -44,14 +62,18 @@ export const openLoginView = () => (dispatch, getState) => {
 
     return auth0
       .auth
-      .userInfo({ token: accessToken})
+      .userInfo({ token: accessToken })
   })
   .then(profile => {
     const userFields = {
       profilePicture: profile.picture,
       name: profile.name,
-      selectedTeam: teams.getIn([0, 'id'], 1)
+      selectedTeam: 1,
     };
+
+    // Create userToken from email
+    const email = profile.emailVerified ? profile.email+''  : null;
+    tokens.userToken = md5((email || '').toLowerCase());
 
     // Save profile to state
     // (we don't have user id yet, because user is not created)
@@ -60,47 +82,47 @@ export const openLoginView = () => (dispatch, getState) => {
       // Save profile to Storage
       AsyncStorage.setItem(STORAGE_KEYS.token, JSON.stringify(tokens), () => {
         // Set storage info to state
+        // needed for user creation already (putUser)
         dispatch(setTokenToStore(tokens));
 
-        // Send profile info to server
-        // and then get created user
-        Promise.resolve(dispatch(putUser()))
-        .then(() => dispatch(fetchAppContent()));
-      });
-    })
-  });
-  /*
-  lock.show({
-    connections: ['google-oauth2'],
-    scope: 'openid profile email',
-  }, (err, profile, token) => {
+        // If user already exists
+        dispatch(fetchUser())
+        .then(user => {
+          // If user is not found create user
+          if (!user) {
+            return dispatch(openLoginTeamSelector())
+          }
 
-    const userFields = {
-      profilePicture: profile.picture,
-      name: profile.name,
-      selectedTeam: teams.getIn([0, 'id'], 1)
-    };
-
-    // Save profile to state
-    // (we don't have user id yet, because user is not created)
-    Promise.resolve(dispatch(updateProfile(userFields)))
-    .then(() => {
-      // Save profile to Storage
-      AsyncStorage.setItem(STORAGE_KEYS.token, JSON.stringify(token), () => {
-        // Set storage info to state
-        dispatch(setTokenToStore(token));
-
-
-        // Send profile info to server
-        // and then get created user
-        Promise.resolve(dispatch(putUser()))
-        .then(() => dispatch(fetchAppContent()));
-
+          dispatch(endLoginProcess());
+          return Promise.resolve();
+        })
+        .then(() => dispatch(onLoginReady()))
+        .catch(() => dispatch(onLoginError()))
       });
     });
   });
-  */
 }
+
+// Need to keep flags of login completion in order to make sure user is cretated
+const startLoginProcess = () => dispatch => AsyncStorage.setItem(STORAGE_KEYS.loginInProgress, 'true');
+const endLoginProcess = () => dispatch => AsyncStorage.removeItem(STORAGE_KEYS.loginInProgress);
+
+export const saveInitialTeamSelection = () => dispatch =>
+  dispatch(putUser())
+    .then(dispatch(closeLoginTeamSelector()))
+    .then(dispatch(endLoginProcess()))
+    .then(dispatch(onLoginReady()));
+
+const onLoginReady = () => dispatch => {
+  return dispatch(fetchAppContent()).then(() => dispatch({ type: HIDE_LOGIN_LOADER }));
+}
+
+const onLoginError = () => dispatch => {
+  dispatch(logoutUser());
+  dispatch({ type: SHOW_LOGIN_ERROR })
+  dispatch({ type: HIDE_LOGIN_LOADER });
+}
+
 
 const updateAuthToken = (idToken) => dispatch => {
   dispatch({ type: REFRESH_TOKEN_REQUEST });
@@ -158,25 +180,29 @@ export const refreshAuthToken = () => (dispatch, getState) => {
 export const logoutUser = () => (dispatch) => {
   AsyncStorage.removeItem(STORAGE_KEYS.token, () => {
     dispatch(setTokenToStore(null));
+    AsyncStorage.clear();
   });
 }
 
-export const checkUserLogin = () => (dispatch, getState) => {
-  AsyncStorage.getItem(STORAGE_KEYS.token, (err, token) => {
-    if (!token) {
-      // TODO Add login redirect
-
-      // # No need to show login here
-      //    App Intro is shown when user has not logged and
-      //    from there user has to login in order to continue
-
-    } else {
-      const tokenObj = JSON.parse(token);
-      dispatch(setTokenToStore(tokenObj));
-
-      // Get all app content
-      dispatch(fetchAppContent());
+export const checkUserLogin = () => (dispatch) => {
+  // Check that login is not in progress
+  // This can happen when user has closed app
+  // on login process before user is created but token already is stored
+  AsyncStorage.getItem(STORAGE_KEYS.loginInProgress, (error, loginInProgress) => {
+    if (loginInProgress) {
+      return dispatch(logoutUser());
     }
+
+    // Check token
+    AsyncStorage.getItem(STORAGE_KEYS.token, (err, token) => {
+      if (token) {
+        const tokenObj = JSON.parse(token);
+        dispatch(setTokenToStore(tokenObj));
+
+        // Get all app content
+        dispatch(fetchAppContent());
+      }
+    });
   });
 };
 
@@ -184,6 +210,8 @@ export const checkUserLogin = () => (dispatch, getState) => {
 
 const initialState = fromJS({
   isRefreshingToken: false,
+  isLoginFailed: false,
+  isLoadingAppAuth: false,
 });
 
 export default function auth(state = initialState, action) {
@@ -194,6 +222,18 @@ export default function auth(state = initialState, action) {
     case REFRESH_TOKEN_SUCCESS:
     case REFRESH_TOKEN_FAILURE:
       return state.set('isRefreshingToken', false);
+
+    case SHOW_LOGIN_ERROR:
+      return state.set('isLoginFailed', true);
+
+    case HIDE_LOGIN_ERROR:
+      return state.set('isLoginFailed', false);
+
+    case SHOW_LOGIN_LOADER:
+      return state.set('isLoadingAppAuth', true);
+
+    case HIDE_LOGIN_LOADER:
+      return state.set('isLoadingAppAuth', false);
 
     default:
       return state;
