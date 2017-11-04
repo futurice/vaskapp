925be49ba400868a4fb9907980dea5781f833ac0
Pasi Lampinen <plam@fl027.local>
2017-11-04 12:10:17 +0200

    Webview platform specific

---
+++ app/components/webview/WebViewer.ios.js
@@ -0,0 +1,69 @@
+
+'use strict';
+
+import React, { Component, PropTypes } from 'react';
+import {
+  StyleSheet,
+  View,
+  Platform,
+} from 'react-native';
+
+import WebView from 'react-native-wkwebview-reborn';
+import theme from '../../style/theme';
+import Toolbar from '../calendar/EventDetailToolbar';
+import ScrollHeader from '../common/ScrollHeader';
+const IOS = Platform.OS === 'ios';
+
+
+class WebViewer extends Component {
+
+
+  render() {
+
+    const { hideHeader, navigator } = this.props;
+    let { url, name } = this.props.route;
+
+    if (IOS && url.indexOf('https') < 0) {
+      url = 'https://crossorigin.me/' + url;
+    }
+
+    const showHeader = !hideHeader && !IOS;
+    const topPadding = IOS ? 20 : 0;
+
+    return (
+      <View style={[styles.container, { paddingTop: showHeader ? 0 : topPadding }]}>
+        {showHeader &&
+          <ScrollHeader title={name} color={theme.primary} onIconClick={() => navigator.pop()} /> }
+
+        {url &&
+          <WebView
+            source={{uri: url}}
+            scalesPageToFit={false}
+            style={{ flex: 1 }}
+
+            hideKeyboardAccessoryView={true}
+            allowsInlineMediaPlayback={true}
+            domStorageEnabled={true}
+            thirdPartyCookiesEnabled={true}
+            mixedContentMode={'always'}
+            javaScriptEnabled={true}
+          />
+        }
+      </View>
+    );
+  }
+
+}
+
+WebViewer.propTypes = {
+  url: PropTypes.string,
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+
+export default WebViewer;
