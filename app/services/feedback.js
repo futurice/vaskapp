6b320e261eef0ad6a327ead2ff06d3a7b0006aa4
Pasi Lampinen <plam@fl027.local>
2017-11-08 22:58:04 +0200

    feedback url updated

--- app/services/feedback.js
+++ app/services/feedback.js
@@ -1,12 +1,13 @@
 import { Linking } from 'react-native';
 
-const FEEDBACK_EMAIL_ADDRESS = 'futubohemia@futurice.com';
-const FEEDBACK_EMAIL_SUBJECT = 'Futubohemia Feedback';
+const FEEDBACK_EMAIL_ADDRESS = 'vaskapp@futurice.com';
+const FEEDBACK_EMAIL_SUBJECT = 'Vask feedback';
 
 
 
 function formatMailtoUrl(url = FEEDBACK_EMAIL_ADDRESS, subject = FEEDBACK_EMAIL_SUBJECT) {
-  let feedbackUrl = 'mailto:' + url;
+  const mailtoUrl = url || FEEDBACK_EMAIL_ADDRESS;
+  let feedbackUrl = 'mailto:' + mailtoUrl;
   // Subject (ID-hashtag to help searching from Flowdock inbox)
   feedbackUrl += '?subject=' + subject;
   // Body
