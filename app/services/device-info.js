7727f77a9e9e427a99912804f730a3672bb9f220
Pasi Lampinen <plam@fl027.local>
2017-11-08 22:54:26 +0200

    iphonex helper to device-info

--- app/services/device-info.js
+++ app/services/device-info.js
@@ -1,4 +1,6 @@
 import { Dimensions, Platform } from 'react-native';
+import { isIphoneX as _isIphoneX } from 'react-native-iphone-x-helper';
+
 const { height, width } = Dimensions.get('window');
 
 // IOS or Anrdoid
@@ -8,6 +10,9 @@ export const IOS = Platform.OS === 'ios';
 // Ipad
 const aspectRatio = height/width;
 export const isIpad = aspectRatio <= 1.6;
+
+// Iphone X
+export const isIphoneX = _isIphoneX();
 
 
 
