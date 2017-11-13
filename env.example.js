23a45547252417da061d30ef06d77d7f50e6a744
Pasi Lampinen <plam@fl027.local>
2017-11-14 00:28:29 +0200

    env example updated

--- env.example.js
+++ env.example.js
@@ -4,5 +4,16 @@
 
 module.exports = {
   API_TOKEN: '',
-  API_URL: ''
+  API_URL: '',
+
+  // Key for saving AsyncStorage
+  APP_STORAGE_KEY: 'vaskapp',
+
+  // # manage.auth0.com
+  AUTH0_CLIENTID: '',
+  AUTH0_DOMAIN: '',
+
+  // # rss2json.com
+  RSS_API_KEY: 'fjehiayzsf5gdjxqaluxmoyc6z3vlormnsflvso0',
+};
 };
