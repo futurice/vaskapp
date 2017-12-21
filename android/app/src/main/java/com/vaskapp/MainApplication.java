1657e427200cf485bb2ebda115e1a24a9b76f70e
Pasi Lampinen <plam@fl027.local>
2017-12-21 15:36:13 +0200

    Install Android location dialog

--- android/app/src/main/java/com/vaskapp/MainApplication.java
+++ android/app/src/main/java/com/vaskapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.vaskapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.auth0.react.A0Auth0Package;
 import com.heanoria.library.reactnative.locationenabler.RNAndroidLocationEnablerPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LocationServicesDialogBoxPackage(),
             new MapsPackage(),
             new A0Auth0Package(),
             new RNAndroidLocationEnablerPackage(),
